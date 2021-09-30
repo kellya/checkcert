8ddf5f1b49db26e8594583c2efdb4bd474a9d8dd
Alex Kelly <alex.kelly@franklin.edu>
2021-09-30 12:36:53 -0400

    rewrote utilizing a sock-based github gist I found

--- checkcert/checkcert.py
+++ checkcert/checkcert.py
@@ -1,9 +1,96 @@
-import ssl
-import sys
+# heavily modified version of https://gist.githubusercontent.com/gdamjan/55a8b9eec6cf7b771f92021d93b87b2c/raw/d8dc194ec4d0187f985a57138019d04e3a59b51f/ssl-check.py
+# to give a CLI for passing the hosts to check and other optional output
 import click
-import M2Crypto
+from OpenSSL import SSL
+from OpenSSL import crypto
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+import idna
+import sys
+
+from socket import socket
+from collections import namedtuple
 
 __version__ = "0.1.0"
+
+HostInfo = namedtuple(field_names="cert hostname peername", typename="HostInfo")
+
+
+def verify_cert(cert, hostname):
+    # verify notAfter/notBefore, CA trusted, servername/sni/hostname
+    cert.has_expired()
+    # service_identity.pyopenssl.verify_hostname(client_ssl, hostname)
+    # issuer
+
+
+def get_certificate(hostname, port):
+    hostname_idna = idna.encode(hostname)
+    sock = socket()
+
+    try:
+        sock.connect((hostname, port))
+        peername = sock.getpeername()
+        ctx = SSL.Context(SSL.SSLv23_METHOD)  # most compatible
+        ctx.check_hostname = False
+        ctx.verify_mode = SSL.VERIFY_NONE
+        sock_ssl = SSL.Connection(ctx, sock)
+        sock_ssl.set_connect_state()
+        sock_ssl.set_tlsext_host_name(hostname_idna)
+        sock_ssl.do_handshake()
+        cert = sock_ssl.get_peer_certificate()
+        crypto_cert = cert.to_cryptography()
+        sock_ssl.close()
+        sock.close()
+    except ConnectionRefusedError:
+        pass
+
+    return HostInfo(cert=crypto_cert, peername=peername, hostname=hostname)
+
+
+def get_alt_names(cert):
+    try:
+        ext = cert.extensions.get_extension_for_class(x509.SubjectAlternativeName)
+        return ext.value.get_values_for_type(x509.DNSName)
+    except x509.ExtensionNotFound:
+        return None
+
+
+def get_x509_text(cert):
+    return crypto.dump_certificate(crypto.FILETYPE_TEXT, cert)
+
+
+def get_common_name(cert):
+    try:
+        names = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
+        return names[0].value
+    except x509.ExtensionNotFound:
+        return None
+
+
+def get_issuer(cert):
+    try:
+        names = cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)
+        return names[0].value
+    except x509.ExtensionNotFound:
+        return None
+
+
+def print_basic_info(hostinfo):
+    print(
+        f"""
+{hostinfo.hostname} ({hostinfo.peername[0]}:{hostinfo.peername[1]})
+\tcommonName: {get_common_name(hostinfo.cert)}
+\tSAN: {get_alt_names(hostinfo.cert)}
+\tissuer: {get_issuer(hostinfo.cert)}
+\tnotBefore: {hostinfo.cert.not_valid_before}
+\tnotAfter:  {hostinfo.cert.not_valid_after}
+          """
+    )
+
+
+def check_it_out(hostname, port):
+    hostinfo = get_certificate(hostname, port)
+    print_basic_info(hostinfo)
 
 
 @click.command()
@@ -12,29 +99,36 @@ __version__ = "0.1.0"
 @click.option(
     "--dump", is_flag=True, help="Dump the full text version of the x509 certificate"
 )
-@click.option(
-    "--port", default=443, type=int, help="TCP port to connect to (default 443)"
-)
 @click.option("--expires", is_flag=True, help="Display the expiration date")
-@click.argument("domain")
-def main(san, dump, port, expires, domain):
+@click.argument("hosts", nargs=-1)
+def main(san, dump, expires, hosts):
+    # setup the list of tuples
+    HOSTS = []
     # handle a domain given with a : in it to specify the port
-    if ":" in domain:
-        uri = domain.split(":")
-        domain = uri[0]
-        port = uri[1]
-    cert = ssl.get_server_certificate((domain, port))
-    x509 = M2Crypto.X509.load_cert_string(cert)
-    if dump:
-        print(x509.as_text())
-        sys.exit()
-    if san:
-        all_sans = x509.get_ext("subjectAltName").get_value()
-        sans = all_sans.split(",")
-        for san in sans:
-            print(str(san).strip().removeprefix("DNS:"))
-        print(x509.get_subject().as_text())
-    print(f"Certificate for {domain}\nexpires after: {x509.get_not_after()}")
+    for host in hosts:
+        # if  a host has a : in it, split on the :, first field will be host
+        # second field will be the port
+        if ":" in host:
+            host_info = host.split(":")
+            HOSTS.append((host_info[0], int(host_info[1])))
+        else:
+            HOSTS.append((host, 443))
+    output_string = ""
+    for hostinfo in map(lambda x: get_certificate(x[0], x[1]), HOSTS):
+        if dump:
+            print(get_x509_text(hostinfo.cert).decode())
+        else:
+            output_string += (
+                f"{hostinfo.hostname} ({hostinfo.peername[0]}:{hostinfo.peername[1]})\n"
+            )
+            output_string += f"\tcommonName: {get_common_name(hostinfo.cert)}\n"
+            if san:
+                output_string += f"\tSAN: {get_alt_names(hostinfo.cert)}\n"
+            output_string += f"\tissuer: {get_issuer(hostinfo.cert)}\n"
+            output_string += f"\tnotBefore: {hostinfo.cert.not_valid_before}\n"
+            output_string += f"\tnotAfter:  {hostinfo.cert.not_valid_after}\n\n"
+            print(output_string)
+    # print(f"Certificate for {domain}\nexpires after: {x509.get_not_after()}")
 
 
 if __name__ == "__main__":
